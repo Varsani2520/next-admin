92b45da3a58a538597911f5d71af2df74cfa80ac
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-17 11:28:01 +0530

    Added Home Provider With Pagniations

--- app/components/Home/Analytics.jsx
+++ app/components/Home/Analytics.jsx
@@ -3,7 +3,7 @@
 import OrderChart from "@/app/common/OrderChart";
 import ProviderCard from "@/app/common/ProviderCard";
 import { GetHomeScreen } from "@/app/services/GetHomeScreen";
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const Analytics = () => {
@@ -15,6 +15,8 @@ const Analytics = () => {
   ];
 
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
 
   async function GetHomeScreenData() {
     try {
@@ -30,6 +32,15 @@ const Analytics = () => {
     GetHomeScreenData();
   }, []);
 
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+  // (1 - 1) * 3, (1 * 3) = 0, 3
+  const displayedData = data.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
   return (
     <div className="">
       <Grid container>
@@ -42,13 +53,21 @@ const Analytics = () => {
           <div className="p-3 bg-white rounded-2xl">
             <h1 className="text-2xl font-semibold">Top Providers</h1>
             <Grid container className="mt-2">
-              {data &&
-                data.map((provider) => (
-                  <Grid item xs={12} md={4}>
-                    <ProviderCard key={provider.id} provider={provider} />
+              {displayedData &&
+                displayedData.map((provider) => (
+                  <Grid item xs={12} md={4} key={provider.id}>
+                    <ProviderCard provider={provider} />
                   </Grid>
                 ))}
             </Grid>
+            <div className="text-right flex justify-end w-full">
+              <Pagination // 10 / 3 = 3
+                count={Math.ceil(data.length / itemsPerPage)}
+                page={currentPage} // 1 PAGE
+                variant="text"
+                onChange={handlePageChange}
+              />
+            </div>
           </div>
         </Grid>
       </Grid>
