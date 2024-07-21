ff55a79c11dd6efe51bc886273401def1669b593
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-21 10:33:26 +0530

    reusable

--- app/common/CustomButton.jsx
+++ app/common/CustomButton.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import Button from '@mui/material/Button'
+import React from "react";
+import Button from "@mui/material/Button";
 
 const CustomButton = ({
-    onClick,
-    variant,
-    color,
-    size,
-    startIcon,
-    endIcon,
-    style,
-    className,href,
-    title }) => {
-    // Define valid color options
-    const validColors = ['default', 'inherit', 'primary', 'secondary', 'success', 'error', 'info', 'warning']
+  onClick,
+  variant,
+  color,
+  size,
+  startIcon,
+  endIcon,
+  style,
+  className,
+  href,
+  title,
+}) => {
+  // Define valid color options
+  const validColors = [
+    "default",
+    "inherit",
+    "primary",
+    "secondary",
+    "success",
+    "error",
+    "info",
+    "warning",
+  ];
 
-    return (
-        <Button className="mr-2"
-            onClick={onClick}
-            variant={variant}
-            color={validColors.includes(color) ? color : 'primary'} // Ensure color is valid
-            size={size}
-            startIcon={startIcon}
-            endIcon={endIcon}
-            style={style}href={href}
-        >{title}
-        </Button>
-    )
-}
+  return (
+    <Button
+      className={"mr-2" + "" + className}
+      onClick={onClick}
+      variant={variant}
+      color={validColors.includes(color) ? color : "primary"} // Ensure color is valid
+      size={size}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      style={style}
+      href={href}
+    >
+      {title}
+    </Button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
