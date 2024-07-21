fcf23ebc733daf354754b93d81d859fdc7e8b5e5
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-21 11:03:20 +0530

    cart layout

--- app/cart/layout.js
+++ app/cart/layout.js
@@ -3,7 +3,7 @@ import BreadCrumb from "../common/BreadCrumb";
 
 const layout = ({ children }) => {
   return (
-    <div>
+    <div className="">
       <div className="bg-white mt-4 p-4 rounded-2xl">
         <h1 className="font-bold text-2xl">Cart</h1>
         <BreadCrumb title={"Cart"} />
